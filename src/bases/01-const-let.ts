0e68e292f9e31bc7b3c6c9db4589175bb8cdfff2
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-08-15 14:32:08 -0500

    Agrega el archivo 01-const-let.ts y actualiza main.ts para importar el nuevo módulo.

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+const firstName: string = "Robinson";
+const lastName: string = "Betancur Marin";
+
+let diceNumber = 5;
+diceNumber = 3;
+console.log("🚀 ~ diceNumber:", diceNumber)
+
+const containsLetterH = lastName.includes('Beta')
+
+console.log({containsLetterH, diceNumber, firstName, lastName});
