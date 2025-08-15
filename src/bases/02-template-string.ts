af1bb28ed2c8f94cf6aa44463774deac2ccedda0
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-08-15 14:39:42 -0500

    template strings

---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,7 @@
+const firstName: string = "Robinson";
+const lastName: string = "Betancur Marin";
+
+const fullName: string = `${firstName} ${lastName}`;
+
+console.log(firstName + " " + lastName);
+console.log(fullName);
