66dc035938eee4349494dcbb7d09f26d59d3aaa6
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-08-22 09:32:49 -0500

    Actualiza 03-object-literal.ts para comentar ejemplos de clonación de objetos y definir la interfaz Person

--- src/bases/03-object-literal.ts
+++ src/bases/03-object-literal.ts
@@ -1,24 +1,51 @@
-const ironman = {
-    firstName: "Tony",
-    lastName: "Stark",
-    age: 45,
-    address: {
-        street: "10880 Malibu Point",
-        city: "Malibu",
-        state: "California"
-    }
+// const ironman = {
+//     firstName: "Tony",
+//     lastName: "Stark",
+//     age: 45,
+//     address: {
+//         street: "10880 Malibu Point",
+//         city: "Malibu",
+//         state: "California"
+//     }
+// }
+
+// // const spiderman = {...ironman}; // rompe la diferencia de referencia a primer nivel
+
+// const spiderman = structuredClone(ironman); // permite clonar a profundidad (depthClone)
+
+// spiderman.firstName = "Peter";
+// spiderman.lastName = "Parker";
+// spiderman.age = 28;
+// spiderman.address.city = "New York";
+
+// console.log(ironman, spiderman);
+
+// ----------------------------
+
+interface Person {
+  address?: Address;
+  age: number;
+  firstName: string;
+  lastName: string;
 }
 
-// const spiderman = {...ironman}; // rompe la diferencia de referencia a primer nivel
+interface Address {
+  city: string;
+  postalCode: string;
+  state: string;
+  street: string;
+}
 
-const spiderman = structuredClone(ironman); // permite clonar a profundidad (depthClone) 
+const ironman: Person = {
+  firstName: "Tony",
+  lastName: "Stark",
+  age: 45,
+  address: {
+    city: "Malibu",
+    postalCode: "90265",
+    state: "California",
+    street: "10880 Malibu Point",
+  },
+};
 
-spiderman.firstName = "Peter";
-spiderman.lastName = "Parker";
-spiderman.age = 28;
-spiderman.address.city = "New York";
-
-console.log(ironman, spiderman);
-
-
-
+console.log(ironman);
