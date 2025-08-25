d272ded5afab8678241a9566bccbe9927ae924aa
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-08-25 12:28:17 -0500

    Actualiza 04-arrays.ts para descomentar la declaración de myArray y agregar myArray3 usando structuredClone

--- src/bases/04-arrays.ts
+++ src/bases/04-arrays.ts
@@ -1,3 +1,4 @@
+// const myArray: (number | string)[] = [1, 2, 3, 4, 5, 6];
 const myArray: number[] = [1, 2, 3, 4, 5, 6];
 
 // myArray.push(10);
@@ -9,7 +10,8 @@ const myArray: number[] = [1, 2, 3, 4, 5, 6];
 //   console.log(myNumber + 10);
 // }
 
-const myArray2 = [...myArray]
+const myArray2 = [...myArray];
+const myArray3 = structuredClone(myArray);
 
-myArray2.push(7)
-console.log({myArray, myArray2});
+myArray2.push(7);
+console.log({ myArray, myArray2, myArray3 });
