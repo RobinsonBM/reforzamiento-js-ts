dc157a330921cd772871a203971379452590c071
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-09-05 12:45:36 -0500

    Agrega la interfaz GiphyRandomResponse y sus dependencias en giphy.response.ts. Refactoriza 10-fetch-api.ts para utilizar createImgInsideDOM.

--- src/bases/10-fetch-api.ts
+++ src/bases/10-fetch-api.ts
@@ -1,20 +1,23 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = "dr6TG1Y8aoGhaTIZjr5QiDy6NAaWgHqs";
 
 const myRequest = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
 );
 
+const createImgInsideDOM = (url: string) => {
+  const imgElement = document.createElement("img");
+  imgElement.src = url;
+
+  document.body.append(imgElement);
+};
+
 myRequest
   .then((response) => response.json())
-  .then(({ data }) => {
+  .then(({ data }: GiphyRandomResponse) => {
     const { url } = data.images.original;
-    console.log(url);
-
-    const imgElement = document.createElement("img");
-    imgElement.src = url
-
-    document.body.append(imgElement)
-
+    createImgInsideDOM(url);
   })
   .catch((err) => console.error(err))
   .finally();
